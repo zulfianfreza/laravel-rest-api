3bcdf39926ee6d45b7c9ca2423351cd7523b91e5
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2024-01-09 11:04:42 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\ApiAuthMiddleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post("/users", [UserController::class, 'register']);
+Route::post("/users/login", [UserController::class, 'login']);
+
+Route::middleware(ApiAuthMiddleware::class)->group(function () {
+  Route::get('/users/current', [UserController::class, 'get']);
+});
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Database\Seeders\UserSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+  /**
+   * A basic feature test example.
+   */
+  public function testRegisterSuccess()
+  {
+    $this->post('/api/users', [
+      "username" => "julian",
+      "password" => "julian",
+      "name" => "Julian Reza"
+    ])->assertStatus(201)->assertJson([
+      "data" => [
+        "username" => "julian",
+        "name" => "Julian Reza"
+      ]
+    ]);
+  }
+
+  public function testRegisterFailed()
+  {
+    $this->post('/api/users', [
+      "username" => "",
+      "password" => "",
+      "name" => ""
+    ])->assertStatus(400)->assertJson([
+      "errors" => [
+        "username" => [
+          "The username field is required."
+        ],
+        "password" => [
+          "The password field is required."
+        ],
+        "name" => [
+          "The name field is required."
+        ]
+      ]
+    ]);
+  }
+
+  public function testRegisterUsernameAlreadyExists()
+  {
+    $this->testRegisterSuccess();
+    $this->post('/api/users', [
+      "username" => "julian",
+      "password" => "julian",
+      "name" => "Julian Reza"
+    ])->assertStatus(400)->assertJson([
+      "errors" => [
+        "username" => [
+          "username already registered"
+        ],
+      ]
+    ]);
+  }
+
+  public function testLoginSuccess()
+  {
+    $this->seed([UserSeeder::class]);
+    $this->post('/api/users/login', [
+      "username" => "test",
+      "password" => "test",
+    ])->assertStatus(200)->assertJson([
+      "data" => [
+        "username" => "test",
+        "name" => "test"
+      ]
+    ]);
+
+    $user = User::where('username', 'test')->first();
+    self::assertNotNull($user->token);
+  }
+
+  public function testLoginFailedUsernameNotFound()
+  {
+    $this->post('/api/users/login', [
+      "username" => "test",
+      "password" => "test",
+    ])->assertStatus(401)->assertJson([
+      "errors" => [
+        "message" => [
+          "username or password wrong"
+        ],
+      ]
+    ]);
+  }
+
+  public function testLoginFailedPasswordWrong()
+  {
+    $this->seed([UserSeeder::class]);
+    $this->post('/api/users/login', [
+      "username" => "test",
+      "password" => "salah",
+    ])->assertStatus(401)->assertJson([
+      "errors" => [
+        "message" => [
+          "username or password wrong"
+        ],
+      ]
+    ]);
+  }
+
+  public function testGetSuccess()
+  {
+    $this->seed([UserSeeder::class]);
+
+    $this->get('/api/users/current', [
+      'Authorization' => 'test'
+    ])->assertStatus(200)->assertJson([
+      'data' => [
+        'username' => 'test',
+        'name' => 'test'
+      ]
+    ]);
+  }
+
+  public function testGetUnauthorized()
+  {
+    $this->seed([UserSeeder::class]);
+
+    $this->get('/api/users/current')->assertStatus(401)->assertJson([
+      'errors' => [
+        'message' => [
+          'unauthorized'
+        ]
+      ]
+    ]);
+  }
+
+  public function testGetInvalidToken()
+  {
+    $this->seed([UserSeeder::class]);
+
+    $this->get('/api/users/current', [
+      'Authorization' => 'salah'
+    ])->assertStatus(401)->assertJson([
+      'errors' => [
+        'message' => [
+          'unauthorized'
+        ]
+      ]
+    ]);
+  }
+}
