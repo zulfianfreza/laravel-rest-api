e40407bfe79c7a77eced869b4ea80330dde11c9e
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2024-01-09 14:58:24 +0700

    feat: update contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactCreateRequest;
+use App\Http\Requests\ContactUpdateRequest;
 use App\Http\Resources\ContactResource;
 use App\Models\Contact;
 use Illuminate\Http\Exceptions\HttpResponseException;
@@ -38,6 +39,30 @@ class ContactController extends Controller
         ]
       ])->setStatusCode(404));
     }
+
+    return new ContactResource($contact);
+  }
+
+  public function update(int $id, ContactUpdateRequest $request): ContactResource
+  {
+    $user = Auth::user();
+
+    $contact = Contact::where('id', $id)->where('user_id', $user->id)->first();
+
+    if (!$contact) {
+      throw new HttpResponseException(response()->json([
+        'errors' => [
+          'message' => [
+            'not found'
+          ]
+        ]
+      ])->setStatusCode(404));
+    }
+
+    $data = $request->validated();
+
+    $contact->fill($data);
+    $contact->save();
 
     return new ContactResource($contact);
   }
--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -115,4 +115,49 @@ class ContactTest extends TestCase
         ]
       ]);
   }
+
+  public function testUpdateSuccess()
+  {
+    $this->seed([UserSeeder::class, ContactSeeder::class]);
+    $contact = Contact::query()->limit(1)->first();
+
+    $this->put('/api/contacts/' . $contact->id, [
+      'first_name' => 'test2',
+      'last_name' => 'test2',
+      'email' => 'test2@julianreza.com',
+      'phone' => '6281234567890',
+    ], [
+      'Authorization' => 'test'
+    ])->assertStatus(200)
+      ->assertJson([
+        'data' => [
+          'first_name' => 'test2',
+          'last_name' => 'test2',
+          'email' => 'test2@julianreza.com',
+          'phone' => '6281234567890',
+        ]
+      ]);
+  }
+
+  public function testUpdateValidationError()
+  {
+    $this->seed([UserSeeder::class, ContactSeeder::class]);
+    $contact = Contact::query()->limit(1)->first();
+
+    $this->put('/api/contacts/' . $contact->id, [
+      'first_name' => '',
+      'last_name' => 'test2',
+      'email' => 'test2@julianreza.com',
+      'phone' => '6281234567890',
+    ], [
+      'Authorization' => 'test'
+    ])->assertStatus(400)
+      ->assertJson([
+        'errors' => [
+          'first_name' => [
+            'The first name field is required.'
+          ],
+        ]
+      ]);
+  }
 }
