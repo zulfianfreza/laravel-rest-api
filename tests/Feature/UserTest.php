5a4858e16a737d96386fd6ac86a1f056c403a78b
Zulfian Fachru Reza <zulfian.025fr@gmail.com>
2024-01-09 11:19:38 +0700

    feat: update user endpoint

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -151,4 +151,58 @@ class UserTest extends TestCase
       ]
     ]);
   }
+
+  public function testUpdateNameSuccess()
+  {
+    $this->seed([UserSeeder::class]);
+    $oldUser = User::where('username', 'test')->first();
+
+    $this->patch('/api/users/current', [
+      'password' => 'baru'
+    ], [
+      'Authorization' => 'test'
+    ])->assertStatus(200)->assertJson([
+      'data' => [
+        'username' => 'test',
+        'name' => 'test'
+      ]
+    ]);
+
+    $newUser = User::where('username', 'test')->first();
+    self::assertNotEquals($oldUser->password, $newUser->password);
+  }
+  public function testUpdatePasswordSuccess()
+  {
+    $this->seed([UserSeeder::class]);
+    $oldUser = User::where('username', 'test')->first();
+
+    $this->patch('/api/users/current', [
+      'name' => 'Julian'
+    ], [
+      'Authorization' => 'test'
+    ])->assertStatus(200)->assertJson([
+      'data' => [
+        'username' => 'test',
+        'name' => 'Julian'
+      ]
+    ]);
+
+    $newUser = User::where('username', 'test')->first();
+    self::assertNotEquals($oldUser->name, $newUser->name);
+  }
+  public function testUpdateFailed()
+  {
+    $this->seed([UserSeeder::class]);
+    $this->patch('/api/users/current', [
+      'name' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis totam dolorum suscipit saepe voluptatem vel ex maiores distinctio nihil et laboriosam quia nesciunt pariatur, modi laudantium? A, rem dicta non nulla accusamus distinctio, reprehenderit expedita possimus explicabo fugiat, molestiae architecto? A aliquam eligendi voluptate omnis nesciunt impedit aperiam optio, placeat possimus velit recusandae alias quaerat beatae dolor laborum blanditiis quibusdam maiores qui tempore deleniti dicta? Dolorum, unde blanditiis at fugiat ipsam tempore repudiandae sapiente laboriosam neque quidem dignissimos dolores ullam recusandae maxime veritatis dolor. Corrupti omnis minus quo? Modi ex sit libero iste id delectus incidunt blanditiis culpa maiores nisi.'
+    ], [
+      'Authorization' => 'test'
+    ])->assertStatus(400)->assertJson([
+      'errors' => [
+        'name' => [
+          'The name field must not be greater than 100 characters.'
+        ]
+      ]
+    ]);
+  }
 }
